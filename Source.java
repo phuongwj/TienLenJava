66b2c173792b12da8991dfef95aec8b148110da2
phuongwj <phamjulia9@gmail.com>
2024-08-01 17:30:24 -0300

    updates on brainstormed ideas

--- Source.java
+++ Source.java
@@ -10,6 +10,9 @@ public class Source {
         Player playerSam = new Player();
         Player playerSi = new Player();
 
+        // An array of Players initialization.
+        Player[] player = new Player[4];
+
         for (int i = 0; i < 13; i++) {
             playerNeung.addDeckShuffled(i);
         }
@@ -41,83 +44,16 @@ public class Source {
         System.out.println("After sorted:");
         System.out.println();
         deck.printCards(playerNeung, playerSam, playerSi, playerSom);
-
-        // Find 3 of spades.
-        // deck.findThreeOfSpades(null, null, null, null, null, args);
-
-        // Cards on the table initialization.
-        // ArrayList<String> cardsPlayed = new ArrayList<>();
-
-        // System.out.println("Player's deck of cards update:");
-        // printCards(playerOne, playerTwo, playerThree, playerFour);
     }
 
+    // Can consider where there are atleast two players playing against each other. This is for further notice 
+    // when you have options for multiplayers in the game.
 
-    // 1. find 3 of spades method:
-    // - go through all of the player's deck, 
-    //  + compare each every element of that player's deck to the string "3 of Spades" to see which player has it. 
-    //  + if we have found the player then the "3 of Spades" from that player's deck will be gone.
-    // - deleted card will be printed.
-    // - print the cards from the players again.
-
-    // SWITCH EVERYTHING TO OBJECTS NOW.
-    // 2. move the turn to the next player
-    // 3. player dealts card if the card they wanna deal is larger than the card before it.
-    // 4. show the card that has been dealt
-    // 5. update the player's card deck
-    // 6. move the turn to the next player and keep going for the cycle.
-
-    // extra brainstorm done: when you check the card that is on the table and the card that the player wanna deal, you would only 
-    // need two Strings, an array is unnecessary cause you're comparing those two Strings. the card of the player can only be dealt 
-    // if is bigger than the other one.
-    // you can either use your turn to play the card or pass it to the next person.
-    // only show the last two cards in the cardsPlayed arraylist.
-    // find a way to store a whole sort of array? inside arraylist. cause what if a player plays a whole sequence? therefore we cant
-    // use just two strings, cause what if its a whole sequence?
-
-    // ideas for switching this to OOP:
-    // - deck class
-    //  + initialize the deck in there, have getters so you can have access to the indexes
-    //  + shuffle cards method
-    //  + sort cards method
-
-    // what can that player class has so that we can create player instances?
-    // - create a class for players. then inside the Source.java, make 4 instances from that class.
-    //  + Player player1 = new Player() etc,.
-    //  + the Player class will hold an array list of strings. 
-    //    => we will have a method that can add the cards into the Player's array list of string.
-    //    => we will also have a method that can remove the cards from the Player's array list of string.
-    //  + find 3 of Spades method
-    //
-
-    // Check discord message via michael.
-    // Can consider where there are two players. This is for further notice when you have options for 
-    // multiplayers in the game.
-//     LENGTH 4 (AKA 4 PLAYERS):
-// - If 3 of Spades is at index 0: keeps still
-
-// - If 3 of Spades is at index 1:
-// (1 % 4) - 1 = 1 - 1 = 0 (move to index 0)
-// (2 % 4) - 1 = 2 - 1 = 1 (move to index 1)
-// (3 % 4) - 1 = 3 - 1 = 2 (move to index 2)
-// + if previous index = 0 => move to index at array.length() - 1 + 0 which means 3
-
-// - If 3 of Spades is at index 2:
-// (2 % 4) - 2 = 2 - 2 = 0 (move to index 0)
-// (3 % 4) - 2 = 3 - 2 = 1 (move to index 1)
-// + if previous index = 0 => move to index at array.length() - 2 + 0 which means 2
-// + if preivous index = 1 => move to index at array.length() - 2 + 1 which means 3
-
-// - If 3 of Spades is at index 3:
-// (3 % 4) - 3 = 3 - 3 = 0 (move to index 0)
-// + if previous index = 0 => move to index at array.length() - 3 + 0 which means 1
-// + if previous index = 1 => move to index at array.length() - 3 + 1 which means 2
-// + if previous index = 2 => move to index at array.length() - 3 + 2 which meas 3
-
-// LENGTH 2 (AKA 2 PLAYERS):
-// - If 3 of Spades is at index 0: keeps still
-
-// - If 3 of Spades is at index 1:
-// (1 % 2) - 1 = 1 - 1 = 0 (move to index 0)
-// + if previous index = 0 => move to index at array.length() - 1 + 0 which means 1
+    // 1. Switch the individual Player to an array that can hold classes.
+    //   + create method that can remove the cards from the Player's array list of strings.
+    // 2. Find 3 of Spades method only for the first game.
+    // 3. Do the rotation thingy.
+    // 4. Deal the cards.
+    // 5. Figure out how you would keep track of who finishes the game before first so that the next game, that 
+    //    person will get to deal the cards first. 
 }
